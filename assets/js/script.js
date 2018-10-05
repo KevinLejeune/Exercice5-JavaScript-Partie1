1a87799ac966f52a2036cb0dbee1be8a789a822d
Kévin Lejeune <lejeune-kevin@hotmail.fr>
2018-10-05 16:50:23 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function multiplication() {
+  var firstNumber = Math.round(document.getElementById("firstNumber").value);
+  var secondNumber = document.getElementById("secondNumber").value;
+
+  alert(firstNumber * secondNumber);
+}
